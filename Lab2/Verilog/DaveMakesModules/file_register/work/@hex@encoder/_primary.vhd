b259285500829ca1da79db548ac92f4885304234
IAN JAMES GILMAN <igilman@uw.edu>
2016-04-17 22:14:49 -0700

    rearrange stuff, remove stuff

--- Lab2/Verilog/DaveMakesModules/file_register/work/@hex@encoder/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity HexEncoder is
-    port(
-        \in\            : in     vl_logic_vector(3 downto 0);
-        hexOut          : out    vl_logic_vector(6 downto 0)
-    );
-end HexEncoder;
