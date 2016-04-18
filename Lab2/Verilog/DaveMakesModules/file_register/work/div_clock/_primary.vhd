b1d7114e1468bf8f3d3679fdcef0e28a2a167c1b
IAN JAMES GILMAN <igilman@uw.edu>
2016-04-17 21:17:30 -0700

    $IJG: removed redundant file entries (mostly) [DO NOT COPY AND CASTE EXTANT VERILOG CODE FILES!!!!]

--- Lab2/Verilog/DaveMakesModules/file_register/work/div_clock/_primary.vhd
+++
@@ -1,8 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity div_clock is
-    port(
-        orig_clk        : in     vl_logic;
-        div_clks        : out    vl_logic_vector(31 downto 0)
-    );
-end div_clock;
