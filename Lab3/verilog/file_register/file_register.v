f8e92cce01c7c6b2341f0cdd3c0d96ba5e9ce3c8
IAN JAMES GILMAN <igilman@uw.edu>
2016-04-30 19:45:27 -0700

    : modified file register in lab3

--- Lab3/verilog/file_register/file_register.v
+++ Lab3/verilog/file_register/file_register.v
@@ -15,22 +15,21 @@ module file_register(
          clk, 
          we,
          re,
-         rs,
          rst,
          read0_addr, 
          read1_addr,
          write_addr,
-         data_bus
+         data_bus,
+         read0_data
        );
    input wire clk, we, re,  // clock, write enable, read enable
-              rs, rst;  // read select, !reset file registers
+              rst;  // !reset file registers
    input wire [4:0] read0_addr,  // read0 register address selection
                      read1_addr,  // read1 register address selection
                      write_addr;  // write register address selection
    inout [31:0] data_bus;  // bus for data read/write
-   wire [31:0] read0_data,  // data from read0_addr 
-               read1_data,  // data from read1_addr
-               read_data_proxy,  // mux read_s based on rs 
+   output wire [31:0] read0_data;  // data from read0_addr
+   wire [31:0] read1_data,  // data from read1_addr 
                write_data;  // data to write to write_addr
    wire read_fr, write_fr,  // read from fr, write to fr
         n_we, n_re;  // !write enable, !read enable
@@ -45,19 +44,7 @@ module file_register(
    // read from file register buffer
    genvar i;
    generate for(i=0; i<32; i=i+1) begin: READ
-      bufif1 read_tri(data_bus[i], read_data_proxy[i], read_fr);
-   end
-   endgenerate
-   
-   // choose which read address to read from
-   genvar j;
-   generate for(j=0; j<32; j=j+1) begin: CHOOSE_READ
-      mux_2to1 read_mux(
-                  .in0(read0_data[j]), 
-                  .in1(read1_data[j]), 
-                  .select(rs), 
-                  .out(read_data_proxy[j])
-               );
+      bufif1 read_tri(data_bus[i], read1_data[i], read_fr);
    end
    endgenerate
    
