8544b2a2b00d491dd30a8d6b28e3395ca0f9d4ca
David W. Dolengewicz <ddolen@uw.edu>
2016-05-13 15:05:42 -0700

    fixed suspendEnable delay in Program_Control

--- Lab4/Verilog/Program_Control/Program_Control.v
+++ Lab4/Verilog/Program_Control/Program_Control.v
@@ -24,7 +24,8 @@ input wire [6:0] writeAddress;
 input wire clk, writeEnable, jump, jumpReg, branch, negative, reset, suspendEnable; //1-bit flags
 output wire [31:0] instruction; // instruction output
 // Internal
-reg [6:0] counter; //Program counter value
+reg [6:0] counter, nextcount; //Program counter value
+reg susHold, wasSE;
 wire [31:0] signExtended;
 
 //connect instruction memory module
@@ -41,45 +42,68 @@ instruction_memory inst_mem(
                         
 sign_extender extender( .in16(instruction[15:0]), .out32(signExtended) );
                         
-always @(posedge clk or negedge reset) begin
+											
+						
+always @(*) begin
    
-   if(!reset)
-      counter <= 7'b0;
-   else if(suspendEnable) begin
-      counter <= counter;  //do nothing
+   if(suspendEnable) begin
+      nextcount <= counter;  //do nothing
+	  wasSE <= 1'b1;
    end 
    else begin
       // Branching Opcodes
       if(jump) begin  //if jumping
          // Branch Greater Than (bgt)
          if(branch & ~negative) begin
-            counter <= counter + instruction[6:0] + 1'b1;
+            nextcount <= counter + instruction[6:0] + 1'b1;
          end
          
          //  NOT NEEDED
          /*if(branch & negative) begin //Branch Less Than
-            counter = counter - instruction[6:0];
+            nextcount = counter - instruction[6:0];
          end */
          
          // Jump Register (jr)
          else if(jumpReg) begin
-            counter <= jumpRegAddr[6:0];
+            nextcount <= jumpRegAddr[6:0];
          end
          
          // Jump (j)
          else begin 
-            counter <= instruction[6:0]; 
+            nextcount <= instruction[6:0]; 
          end
       end
       
       // No Branching
       else begin
-         counter <= counter + 7'b1;  // Increment Program Counter
+         nextcount <= counter + 7'b1;  // Increment Program Counter
       end
-         
+       wasSE <= 1'b0;  
    end
    
-end   
+end  
+
+always @ (posedge clk) begin
+	
+
+
+end
+
+always @ (posedge clk or negedge reset) begin
+
+	if(!reset) begin
+        counter <= 7'b0;
+	end
+	else if(~suspendEnable & wasSE)
+		counter <= counter;
+	else begin
+	    counter <= nextcount;
+    end
+	
+
+end
+
+ 
                         
 endmodule
 
