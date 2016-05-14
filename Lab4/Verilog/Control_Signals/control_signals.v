8032e02e5a1a4b9ba5815c10d13d67d4b7b2b757
David W. Dolengewicz <ddolen@uw.edu>
2016-05-14 12:29:33 -0700

    Updated control_signals style and Program_Control.

--- Lab4/Verilog/Control_Signals/control_signals.v
+++ Lab4/Verilog/Control_Signals/control_signals.v
@@ -30,6 +30,7 @@ module control_signals(
 	
 	parameter NOP = 6'b0;
 	parameter SLLV = 6'b000100;
+	parameter JR = 6'b001000
 	parameter ADD = 6'b100000;
 	parameter SUB = 6'b100010;
 	parameter AND = 6'b100100;
@@ -56,107 +57,163 @@ module control_signals(
 		
 			REG: begin
 				reg_dst = 1;
-				jump = 0;
+				
+				if (alu_function == JR ) begin
+					jump = 1;
+					jump_reg = 1;
+					alu_op = NOP;
+				end else begin
+					jump = 0;
+					jump_reg = 0;
+					alu_op = alu_function;	
+				end
+				
+				branch = 0;
 				cs = 1;
-				mem_to_reg = 0;
-				alu_op = alu_function;
+				oe = oe;
+				rw = rw;
+				mem_to_reg = 0;		
 				alu_src = 0;
-				reg_write = 0; // I forget, is this 1?
+				if(alu_function == NOP || alu_function == JR)
+					reg_write = 0;
+				else
+					reg_write = 1;// I forget, is this 1?
 			end
 			
 			JUMP: begin
+				reg_dst = reg_dst;
 				jump = 1;
 				jump_reg = 0;
 				branch = 0;
 				cs = 1;
-				reg_write = 0;
+				oe = oe;
+				rw = rw;
+				mem_to_reg = mem_to_reg;
+				alu_op = alu_op;
+				alu_src = alu_src;
+				reg_write = 0;			
 			end
 			
 			IBGT: begin
+				reg_dst = reg_dst;
 				jump = 1;
+				jump_reg = jump_reg;
 				branch = 1;
 				cs = 1;
+				oe = oe;
+				rw = rw;
+				mem_to_reg = mem_to_reg;
 				alu_op = SUB;
 				alu_src = 0;
-				reg_write = 0;
+				reg_write = 0;				
+
 			end
 			
 			IADD: begin
+			
 				reg_dst = 0;
 				jump = 0;
+				jump_reg = 0;
+				branch = 0;
 				cs = 1;
+				oe = oe;
+				rw = rw;
 				mem_to_reg = 0;
 				alu_op = ADD;
 				alu_src = 1;
-				reg_write = 1;
+				reg_write = 1;	
+			
 			end
 			
 			ISLT: begin
 				reg_dst = 0;
 				jump = 0;
+				jump_reg = 0;
+				branch = 0;
 				cs = 1;
+				oe = oe;
+				rw = rw;
 				mem_to_reg = 0;
 				alu_op = SLT;
 				alu_src = 1;
-				reg_write = 1;
+				reg_write = 1;	
 				
 			end
 			
 			IAND: begin
 				reg_dst = 0;
 				jump = 0;
+				jump_reg = 0;
+				branch = 0;
 				cs = 1;
+				oe = oe;
+				rw = rw;
 				mem_to_reg = 0;
 				alu_op = AND;
 				alu_src = 1;
-				reg_write = 1;
+				reg_write = 1;	
 				
 			end
 			
 			IOR: begin
 				reg_dst = 0;
 				jump = 0;
+				jump_reg = 0;
+				branch = 0;
 				cs = 1;
+				oe = oe;
+				rw = rw;
 				mem_to_reg = 0;
 				alu_op = OR;
 				alu_src = 1;
-				reg_write = 1;
+				reg_write = 1;	
 				
 			end
 			
 			IXOR: begin
 				reg_dst = 0;
 				jump = 0;
+				jump_reg = 0;
+				branch = 0;
 				cs = 1;
+				oe = oe;
+				rw = rw;
 				mem_to_reg = 0;
 				alu_op = XOR;
 				alu_src = 1;
-				reg_write = 1;
+				reg_write = 1;	
 				
 			end
 			
 			ILW: begin
+			
 				reg_dst = 0;
 				jump = 0;
+				jump_reg = 0;
+				branch = 0;
 				cs = 0;
 				oe = 1;
 				rw = 0;
 				mem_to_reg = 1;
 				alu_op = ADD;
 				alu_src = 1;
-				reg_write = 1;
+				reg_write = 1;	
 				
 			end
 			
 			ISW: begin
+			
+				reg_dst = reg_dst;
 				jump = 0;
+				jump_reg = 0;
+				branch = 0;
 				cs = 0;
 				oe = 0;
 				rw = 1;
 				mem_to_reg = 0;
 				alu_op = ADD;
 				alu_src = 1;
-				reg_write = 0;
+				reg_write = 0;	
 				
 			end
 		
